4d639b4c534b28602cad0041c2292ba06e3c12f5
aatifzia <aatif.dark@gmail.com>
2017-03-25 10:47:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,10 @@
 console.log('Loaded!');
 
-var img = document.getElementById('bullet');
-var marginLeft = 0;
-function moveright() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setinterval(moveRight,100);
+var button = document.getElementById("counter");
+var counter = 0;
+
+    button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
